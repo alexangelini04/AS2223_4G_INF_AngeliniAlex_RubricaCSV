e2b69b88ff4c3e27dcac8794211b650668328d01
alexangelini04 <alex.angelini@studenti.ittsrimini.edu.it>
2022-09-25 17:09:44 +0200

    Sviluppo del codice per la lettura del file csv

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,29 +35,29 @@
             this.cmbCognome = new System.Windows.Forms.ComboBox();
             this.btnVisualizza = new System.Windows.Forms.Button();
             this.lstElenco = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnFile = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtFile
             // 
-            this.txtFile.Location = new System.Drawing.Point(96, 38);
+            this.txtFile.Location = new System.Drawing.Point(132, 38);
             this.txtFile.Name = "txtFile";
-            this.txtFile.Size = new System.Drawing.Size(407, 23);
+            this.txtFile.Size = new System.Drawing.Size(371, 23);
             this.txtFile.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 41);
+            this.label1.Location = new System.Drawing.Point(34, 41);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(25, 15);
+            this.label1.Size = new System.Drawing.Size(92, 15);
             this.label1.TabIndex = 1;
-            this.label1.Text = "File";
+            this.label1.Text = "File CSV Rubrica";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 119);
+            this.label2.Location = new System.Drawing.Point(34, 119);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(60, 15);
             this.label2.TabIndex = 2;
@@ -65,14 +65,18 @@
             // 
             // txtCognome
             // 
-            this.txtCognome.Location = new System.Drawing.Point(132, 116);
+            this.txtCognome.Location = new System.Drawing.Point(100, 116);
             this.txtCognome.Name = "txtCognome";
-            this.txtCognome.Size = new System.Drawing.Size(324, 23);
+            this.txtCognome.Size = new System.Drawing.Size(356, 23);
             this.txtCognome.TabIndex = 3;
             // 
             // cmbCognome
             // 
             this.cmbCognome.FormattingEnabled = true;
+            this.cmbCognome.Items.AddRange(new object[] {
+            "Inizia per:",
+            "Contiene:",
+            "Finisce per:"});
             this.cmbCognome.Location = new System.Drawing.Point(482, 116);
             this.cmbCognome.Name = "cmbCognome";
             this.cmbCognome.Size = new System.Drawing.Size(95, 23);
@@ -86,31 +90,33 @@
             this.btnVisualizza.TabIndex = 5;
             this.btnVisualizza.Text = "Visualizza";
             this.btnVisualizza.UseVisualStyleBackColor = true;
+            this.btnVisualizza.Click += new System.EventHandler(this.btnVisualizza_Click);
             // 
             // lstElenco
             // 
             this.lstElenco.FormattingEnabled = true;
             this.lstElenco.ItemHeight = 15;
-            this.lstElenco.Location = new System.Drawing.Point(52, 238);
+            this.lstElenco.Location = new System.Drawing.Point(34, 238);
             this.lstElenco.Name = "lstElenco";
-            this.lstElenco.Size = new System.Drawing.Size(525, 169);
+            this.lstElenco.Size = new System.Drawing.Size(543, 169);
             this.lstElenco.TabIndex = 6;
             // 
-            // button1
+            // btnFile
             // 
-            this.button1.Location = new System.Drawing.Point(523, 37);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(54, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "...";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnFile.Location = new System.Drawing.Point(523, 38);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(54, 23);
+            this.btnFile.TabIndex = 7;
+            this.btnFile.Text = "...";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(618, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnFile);
             this.Controls.Add(this.lstElenco);
             this.Controls.Add(this.btnVisualizza);
             this.Controls.Add(this.cmbCognome);
@@ -119,7 +125,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtFile);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "                            ";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,6 +140,6 @@
         private ComboBox cmbCognome;
         private Button btnVisualizza;
         private ListBox lstElenco;
-        private Button button1;
+        private Button btnFile;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,42 @@ namespace AS2223_4G_INF_AngeliniAlex_RubricaCSV
 {
     public partial class Form1 : Form
     {
+        public string[] infoPersone = new string[100];
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog finestrafile = new OpenFileDialog();
+
+            //apertura della finestra del file
+            if(finestrafile.ShowDialog() == DialogResult.OK) 
+            { 
+                //una volta selezionato il file, il suo indirizzo viene scritto nella textbox "txtFile"
+                txtFile.Text = finestrafile.FileName;
+            }
+            StreamReader fi = new StreamReader(finestrafile.FileName);
+
+            //legge il numero delle righe presenti nel file e conta quante righe ci sono nel file
+            int righe = File.ReadLines(finestrafile.FileName).Count();
+
+            for(int i = 0; i < righe; i++) 
+            {
+                infoPersone[i] = fi.ReadLine();
+            
+            
+            }
+
+        }
+
+        private void btnVisualizza_Click(object sender, EventArgs e)
+        {
+            //leggo l'opzione del combobox
+            string combobox = cmbCognome.Text;
+            //leggo la textbox Cognome
+            string Cognome = txtCognome.Text;
         }
     }
 }
